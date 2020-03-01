c3d16297059b595a338f16109cea0cc1d9138677
Kémeri Lajos <kemeri.lajos@gmail.com>
2020-03-01 23:43:03 +0100

    KDSVideo login has been refined.

--- KDSVideo/ViewModels/LoginViewModel.cs
+++ KDSVideo/ViewModels/LoginViewModel.cs
@@ -1,7 +1,13 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Net;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Views;
+using SynologyAPI;
+using SynologyAPI.Exception;
+using SynologyRestDAL;
 
 namespace KDSVideo.ViewModels
 {
@@ -14,18 +20,87 @@ namespace KDSVideo.ViewModels
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
+        public LoginViewModel(INavigationService navigationService, IVideoStation videoStation)
+        {
+            _navigationService = navigationService ?? throw new ArgumentNullException(nameof(navigationService));
+            _videoStation = videoStation ?? throw new ArgumentNullException(nameof(videoStation));
+            NavigateCommand = new RelayCommand(() => _navigationService.NavigateTo(ViewModelLocator.MainPageKey));
+
+            LoginCommand = new RelayCommand(Login);
+        }
+
+        private IWebProxy CreateProxy(string proxyUrl)
+        {
+            return string.IsNullOrWhiteSpace(proxyUrl) ? null : new WebProxy(new Uri(proxyUrl));
+        }
+
+        private IWebProxy GetDefaultProxy()
+        {
+            var proxy = WebRequest.GetSystemWebProxy();
+            proxy.Credentials = CredentialCache.DefaultNetworkCredentials;
+            return proxy;
+        }
+
+        private async void Login()
+        {
+            // TODO: Implement DeviceId reload if available
+
+            var success = false;
+            OtpCode = string.Empty;
+ 
+            do
+            {
+                var baseUri = new Uri(Host);
+
+                // TODO: Implement custom, user defined proxy usage
+                var proxy = GetDefaultProxy() ?? CreateProxy("");
+
+                try
+                {
+                    var cts = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+                    var loginInfo = await _videoStation.LoginAsync(baseUri, Account, Password, OtpCode, _deviceId, proxy, cts.Token);
+                    if (RememberMe)
+                    {
+                        _deviceId = loginInfo.DeviceId ?? string.Empty;
+                    }
+                    success = true;
+                }
+                catch (SynoRequestException e)
+                {
+                    RememberMe = false;
+                    OtpCode = string.Empty;
+                    if (e.ErrorCode == ErrorCodes.OneTimePasswordNotSpecified)
+                    {
+                        // TODO: Request new OTP CODE view state
+                    }
+                    else
+                    {
+                        // TODO: Request Host, Account, Password view state 
+                    }
+                }
+                catch
+                {
+                    // ignored (because of OperationCanceledException or other exception)
+                }
+            } while (!success);
+
+            _navigationService.NavigateTo(ViewModelLocator.MainPageKey);
+        }
 
         private readonly INavigationService _navigationService;
+        private readonly IVideoStation _videoStation;
+
+        private string _deviceId = string.Empty;
+        private string _host = string.Empty;
+        private string _account = string.Empty;
+        private string _password = string.Empty;
+        private string _otpCode = string.Empty;
+
+        private bool _rememberMe;
 
         public RelayCommand NavigateCommand { get; }
 
-        public LoginViewModel(INavigationService navigationService)
-        {
-            _navigationService = navigationService;
-            NavigateCommand = new RelayCommand(() => _navigationService.NavigateTo(ViewModelLocator.MainPageKey));
-        }
-
-        private string _host;
+        public RelayCommand LoginCommand { get; }
 
         public string Host
         {
@@ -36,9 +111,7 @@ namespace KDSVideo.ViewModels
                 OnPropertyChanged(nameof(Host));
             }
         }
-
-        private string _account;
-
+        
         public string Account
         {
             get => _account ?? string.Empty;
@@ -48,9 +121,7 @@ namespace KDSVideo.ViewModels
                 OnPropertyChanged(nameof(Account));
             }
         }
-
-        private string _password;
-
+        
         public string Password
         {
             get => _password ?? string.Empty;
@@ -58,6 +129,26 @@ namespace KDSVideo.ViewModels
             {
                 _password = value ?? string.Empty;
                 OnPropertyChanged(nameof(Password));
+            }
+        }
+        
+        public string OtpCode
+        {
+            get => _otpCode;
+            set
+            {
+                _otpCode = value ?? String.Empty;
+                OnPropertyChanged(nameof(OtpCode));
+            }
+        }
+        
+        public bool RememberMe
+        {
+            get => _rememberMe;
+            set
+            {
+                _rememberMe = value;
+                OnPropertyChanged(nameof(RememberMe));
             }
         }
     }
