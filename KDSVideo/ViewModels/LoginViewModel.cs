302cec38e717a442db81dcbfc90032c587be5d2a
Kémeri Lajos <kemeri.lajos@gmail.com>
2020-03-01 21:44:20 +0100

    KDSVideo login UI refinement

--- KDSVideo/ViewModels/LoginViewModel.cs
+++ KDSVideo/ViewModels/LoginViewModel.cs
@@ -1,10 +1,20 @@
-﻿using GalaSoft.MvvmLight.Command;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Views;
 
 namespace KDSVideo.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
         private readonly INavigationService _navigationService;
 
         public RelayCommand NavigateCommand { get; }
@@ -13,6 +23,42 @@ namespace KDSVideo.ViewModels
         {
             _navigationService = navigationService;
             NavigateCommand = new RelayCommand(() => _navigationService.NavigateTo(ViewModelLocator.MainPageKey));
+        }
+
+        private string _host;
+
+        public string Host
+        {
+            get => _host ?? string.Empty;
+            set
+            {
+                _host = value ?? string.Empty;
+                OnPropertyChanged(nameof(Host));
+            }
+        }
+
+        private string _account;
+
+        public string Account
+        {
+            get => _account ?? string.Empty;
+            set
+            {
+                _account = value ?? string.Empty;
+                OnPropertyChanged(nameof(Account));
+            }
+        }
+
+        private string _password;
+
+        public string Password
+        {
+            get => _password ?? string.Empty;
+            set
+            {
+                _password = value ?? string.Empty;
+                OnPropertyChanged(nameof(Password));
+            }
         }
     }
 }
--- KDSVideo/Views/LoginPage.xaml.cs
+++ KDSVideo/Views/LoginPage.xaml.cs
@@ -1,12 +1,57 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 namespace KDSVideo.Views
 {
     public sealed partial class LoginPage : Page
     {
+        private const string TextControlForegroundFocusedKey = "TextControlForegroundFocused";
+
         public LoginPage()
         {
             this.InitializeComponent();
+        }
+
+        private Brush GetTextControlForegroundFocusedBrush() => (Brush)Application.Current.Resources[TextControlForegroundFocusedKey];
+
+        private void Host_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var foreground = GetTextControlForegroundFocusedBrush();
+            HostIcon.Foreground = foreground;
+            HostMoreIcon.Foreground = foreground;
+        }
+
+        private void Host_LostFocus(object sender, RoutedEventArgs e)
+        {
+            HostIcon.Foreground = Host.Foreground;
+            HostMoreIcon.Foreground = Host.Foreground;
+        }
+
+        private void Account_GotFocus(object sender, RoutedEventArgs e)
+        {
+            AccountIcon.Foreground = GetTextControlForegroundFocusedBrush();
+        }
+
+        private void Account_LostFocus(object sender, RoutedEventArgs e)
+        {
+            AccountIcon.Foreground = Account.Foreground;
+        }
+
+        private void Password_GotFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordIcon.Foreground = GetTextControlForegroundFocusedBrush();
+        }
+
+        private void Password_LostFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordIcon.Foreground = Password.Foreground;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("clicked");
         }
     }
 }
